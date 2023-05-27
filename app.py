557686f08ec128ce5861eef676a44b99fef0012a
Zhalhou-dev <80689458+Zhalhou-dev@users.noreply.github.com>
2023-05-27 17:25:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,22 +13,31 @@ def upload_form():
 
 @app.route('/', methods=['POST'])
 def upload_image():
-    operation_selection = request.form['image_type_selection']
+    #write operation_selection  
+    operation_selection=request.forms['image_type_selection']
+    
     image_file = request.files['file']
     filename = secure_filename(image_file.filename)
     reading_file_data = image_file.read()
     image_array = np.fromstring(reading_file_data, dtype='uint8')
+
     decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
 
     if operation_selection == 'gray':
         file_data = make_grayscale(decode_array_to_img)
     elif operation_selection == 'sketch':
         file_data = image_sketch(decode_array_to_img)
-    #write elif condition for oil image
-    elif operation_selection=='oil':
-        file_data = oil_effect(decode_array_to_img)
-    elif operation_selection=='rgb':
-        file_data=rgb_effect(decode_array_to_img)
+    #write elif condition for Water, Invert and HDR image below
+    elif operation_selection =='oil':
+        file_data= oil_effect(decode_array_to_img)
+    elif operation_selection == 'rgb':
+        file_data= rgb_effect(decode_array_to_img)
+    elif operation_selection== 'water':
+        file_data= water_color_effect(decode_array_to_img)
+    elif operation_selection== 'invert':
+        file_data = inverted_effect(decode_array_to_img)
+    elif operation_selection== 'hdr':
+        file_data = hdr_effect(decode_array_to_img)
 
 
     # Ends here
@@ -46,7 +55,6 @@ def make_grayscale(decode_array_to_img):
 
     return output_image
 
-
 def image_sketch(decode_array_to_img):
 
     converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_BGR2GRAY)
@@ -58,23 +66,48 @@ def image_sketch(decode_array_to_img):
 
     return output_img
 
-# Starts Oil Effect function From here
-def oil_effect(decode_array_to_img):
-    oil_img=cv2.xphoto.oilPainting(decode_array_to_img,7,1)
-    status, output_img = cv2.imencode('.PNG', oil_img)
+def oil_effect(decode_array_to_img): #267
+    oil_effect_img = cv2.xphoto.oilPainting(decode_array_to_img, 7, 1)
+    status, output_img = cv2.imencode('.PNG', oil_effect_img)
+
     return output_img
-# Ends Here
-# Starts RGB Effect function From here
+
 def rgb_effect(decode_array_to_img):
-    rgb_img=cv2.cvtColor(decode_array_to_img,cv2.COLOR_BGR2RGB)
-    status, output_img = cv2.imencode('.PNG', rgb_img)
-    return  output_img
+    rgb_effect_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2BGR)
+    status, output_img = cv2.imencode('.PNG', rgb_effect_img)
+
+    return output_img
+# Codes starts for water_color_effect, invert and HDR effect
+def water_color_effect(decode_array_to_img):
+    water_color_img=cv2.stylization(decode_array_to_img,sigma_s=80,sigma_r=0.4)
+    status,output_img=cv2.imencode('.PNG',water_color_img)
+    return output_img
+def inverted_effect(decode_array_to_img):
+    inverted_img=cv2.bitwise_not(decode_array_to_img)
+    status,output_img=cv2.imencode('.PNG',inverted_img)
+    return output_img
+def hdr_effect(decode_array_to_img):
+    hdr_img=cv2.detailEnhance(decode_array_to_img,sigma_s=80,sigma_r=0.2)
+    status,output_img=cv2.imencode('.PNG',hdr_img)
+    return output_img
 
 
 
 
 
-# Ends here
+
+
+
+
+
+
+
+
+
+
+
+
+# Code ends
 
 
 @app.route('/display/<filename>')
